6616a44e2ea95db5dd8ad08c411664e5fe39549f
Abdon <estadia2020@gmail.com>
2023-10-04 15:21:35 -0600

    reset form label remove in editpatientemodal

--- frontend/src/components/organisms/AddNewPatientModal/index.jsx
+++ frontend/src/components/organisms/AddNewPatientModal/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import styles from './AddNewPatientModal.module.css';
-import { useFormik } from 'formik';
+import { useFormik, resetForm } from 'formik';
 import { toast } from 'react-toastify';
 import { useRouter } from 'next/navigation';
 import Modal from '@/components/atoms/Modal';
@@ -21,7 +21,7 @@ export default function AddNewPatientModal({ isOpen, onClose }) {
       ssn: '',
       user_id: userSession.user_id,
     },
-    onSubmit: (values) => {
+    onSubmit: (values, { resetForm }) => {
       toast
         .promise(createPatient(values), {
           pending: 'Loading',
@@ -29,6 +29,7 @@ export default function AddNewPatientModal({ isOpen, onClose }) {
           error: 'Error creating patient 🤯',
         })
         .then(() => {
+          resetForm(); // Esto reiniciará los valores del formulario
           onClose();
         });
     },
